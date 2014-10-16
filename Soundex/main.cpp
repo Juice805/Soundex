f6c29c254dd6e58531329dc51d3083e281adb10c
Justin Oroz <Justin@Justins-MacBook-Pro-3.local>
2014-10-15 20:50:23 -0700

    Initial Commit

---
+++ Soundex/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Soundex
+//
+//  Created by Justin Oroz on 10/15/14.
+//  Copyright (c) 2014 jmoroz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
